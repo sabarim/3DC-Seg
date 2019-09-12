dfd31d3ef3bf7ef188e2b52daf2583287c9ff5c1
sm110193 <sabarinath.mahadevan@rwth-aachen.de>
2019-09-12 13:42:59 +0200

    Updated to work with resnet3d

--- datasets/YoutubeVOS.py
+++ datasets/YoutubeVOS.py
@@ -34,10 +34,11 @@ class YoutubeVOSDataset(DAVIS):
   def create_img_list(self, _imset_f):
     start = time.time()
     pool = mp.Pool(10)
-    results = [pool.apply(self.video_list, args=(line,)) for line in _imset_f]
+    results = [pool.apply(self.video_list, args=(line,)) for line in _imset_f[:100]]
     results = np.array(results)
     self.videos = results[:, 0]
     self.img_list = np.concatenate(results[:, 1])
+    self.reference_list = self.img_list
 
     video_frames = np.array([results[:, 0], results[:, 1]]).transpose()
     self.video_frames.update({video_frames[i][0]:video_frames[i][1] for i in range(len(video_frames))})
@@ -64,10 +65,11 @@ class YoutubeVOSDataset(DAVIS):
     # max_objects = 1
     shape = np.shape(np.array(Image.open(mask_files[0]).convert("P")))
 
+
     print("Time to create image list for video {} is {}".format(_video, time.time() - start))
     return _video, img_list, max_objects, num_frames, shape
 
-  def read_frame(self, shape, video, f, instance_id=None):
+  def read_frame(self, shape, video, f, instance_id=None, support_indices=None):
     # use a blend of both full random instance as well as the full object
     img_file = os.path.join(self.image_dir, video, '{:05d}.jpg'.format(f))
     raw_frames = np.array(Image.open(img_file).convert('RGB')) / 255.
@@ -75,37 +77,48 @@ class YoutubeVOSDataset(DAVIS):
       mask_file = os.path.join(self.mask_dir, video, '{:05d}.png'.format(f))  # allways return first frame mask
       # cleaned annotations do not exist for all sequences. Use normal annotations for these frames
       if os.path.exists(mask_file):
-        raw_mask = imresize(np.array(Image.open(mask_file).convert('P'), dtype=np.uint8), shape,
-                            interp="nearest")
+        raw_mask = np.array(Image.open(mask_file).convert('P'), dtype=np.uint8)
       else:
-       raw_mask = imresize(np.array(Image.open(mask_file.replace("CleanedAnnotations", "Annotations")).convert('P'), dtype=np.uint8), shape,
-                            interp="nearest")
+       raw_mask = np.array(Image.open(mask_file.replace("CleanedAnnotations", "Annotations")).convert('P'), dtype=np.uint8)
     except:
       mask_file = os.path.join(self.mask_dir, video, '00000.png')
       raw_mask = np.array(Image.open(mask_file).convert('P'), dtype=np.uint8)
 
-    raw_masks = (raw_mask == instance_id).astype(np.uint8) if instance_id is not None else raw_mask
+    raw_masks = (raw_mask == instance_id).astype(np.uint8) if instance_id is not None \
+      else (raw_mask != 0).astype(np.uint8)
     tensors_resized = resize({"image": raw_frames, "mask": raw_masks}, self.resize_mode, shape)
 
-    return tensors_resized["image"] / 255.0, tensors_resized["mask"]
+    return tensors_resized["image"] / 255.0, tensors_resized["mask"], tensors_resized["mask"], tensors_resized["mask"]
 
   def get_support_indices(self, index, sequence):
     # in youtube-vos index does not correspond to the file index
     # i = int(os.path.splitext(os.path.basename(self.img_list[file_index]))[0])
-    sample_list = [int(os.path.splitext(os.path.basename(f))[0]) for f in self.video_frames[sequence]
-                   if int(os.path.splitext(os.path.basename(f))[0]) < index]
+    sample_list = [int(os.path.splitext(os.path.basename(f))[0]) for f in self.video_frames[sequence]]
     sample_list.sort()
+    start_index = sample_list.index(index)
+    end_index = min(len(sample_list), start_index + self.max_temporal_gap)
+    sample_list = sample_list[start_index: end_index]
+    support_indices = np.random.choice(sample_list, min(self.temporal_window, len(sample_list)), replace=False)
+    support_indices = np.sort(np.append(support_indices, np.repeat([index],
+                                                                   self.temporal_window - len(support_indices))))
 
-    if len(sample_list) == 0:
-      support_indices = np.repeat([index], self.temporal_window + 2)
-    elif (len(sample_list) - (self.temporal_window + 2)) < 0:
-      support_indices = np.repeat([index], abs(len(sample_list) - (self.temporal_window + 2)))
-      support_indices = np.append(support_indices, sample_list[-1])
-      support_indices = np.append(support_indices, sample_list[:-1])
-    else:
-      support_indices = np.random.choice(np.array(sample_list)[:-1], self.temporal_window, replace=False)
-      support_indices = np.append(support_indices, np.array([index, sample_list[-1]]))
+    # if len(sample_list) == 0:
+    #   support_indices = np.repeat([index], self.temporal_window + 2)
+    # elif (len(sample_list) - (self.temporal_window + 2)) < 0:
+    #   support_indices = np.repeat([index], abs(len(sample_list) - (self.temporal_window + 2)))
+    #   support_indices = np.append(support_indices, sample_list[-1])
+    #   support_indices = np.append(support_indices, sample_list[:-1])
+    # else:
+    #   support_indices = np.random.choice(np.array(sample_list)[:-1], self.temporal_window, replace=False)
+    #   support_indices = np.append(support_indices, np.array([index, sample_list[-1]]))
 
     support_indices.sort()
     print("support indices are {}".format(support_indices))
     return support_indices.astype(np.int)
+
+  def __getitem__(self, item):
+    input_dict = super(YoutubeVOSDataset, self).__getitem__(item)
+    del input_dict['masks_guidance']
+    input_dict['target'] = input_dict['raw_masks']
+    return input_dict
+
