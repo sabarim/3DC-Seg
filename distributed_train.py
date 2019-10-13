Added config file for embedding loss parameters + adjusted the loss computation + other fixes

distributed_train.py
@@ -156,7 +156,7 @@ def compute_loss(criterion, pred, target, target_extra = None):
       loss_extra = bootstrapped_ce_loss(loss_extra)
     if "embedding" in args.losses:
       pred_extra = F.interpolate(pred_extra, scale_factor=(1,8,8), mode='trilinear')
-      loss_extra, _, _ = compute_embedding_loss(pred_extra, target_extra['similarity_ref'].cuda())
+      loss_extra, _, _ = compute_embedding_loss(pred_extra, target_extra['similarity_ref'].cuda(), args.config_path)
 
   # print("loss_extra {}".format(loss_extra))
   loss = loss_mask + loss_extra
@@ -201,7 +201,8 @@ def validate(dataset, model, criterion, epoch, foo):
               'Loss {loss.val:.4f} ({loss.avg:.5f})\t'
               'Loss Extra {loss_extra.val:.4f} ({loss_extra.avg:.5f})\t'
               'IOU {iou.val:.4f} ({iou.avg:.5f})\t'.format(
-          input_dict['info']['name'], i, len(testloader), batch_time=batch_time, loss=losses, iou=ious_video),
+          input_dict['info']['name'], i, len(testloader), batch_time=batch_time, loss=losses, iou=ious_video,
+          loss_extra=losses_extra),
           flush=True)
     print('Sequence {0}\t IOU {iou.avg}'.format(input_dict['info']['name'], iou=ious_video), flush=True)
     ious.update(ious_video.avg)
@@ -223,11 +224,10 @@ if __name__ == '__main__':
     print("Arguments used: {}".format(args), flush=True)
 
     trainset, testset = get_dataset(args)
-    train_sampler = RandomSampler(trainset, replacement=True, num_samples=args.data_sample) \
-      if args.data_sample is not None else \
-      torch.utils.data.distributed.DistributedSampler(trainset, num_replicas=torch.cuda.device_count(),
-                                                      rank=local_rank, shuffle=True)
-    shuffle = True if args.data_sample is None else False
+    #train_sampler = RandomSampler(trainset, replacement=True, num_samples=args.data_sample) \
+    #  if args.data_sample is not None else \
+    #  torch.utils.data.distributed.DistributedSampler(trainset, shuffle=True)
+    #shuffle = True if args.data_sample is None else False
     model = get_model(args, network_models)
 
     # model = FeatureAgg3dMergeTemporal()
@@ -241,6 +241,10 @@ if __name__ == '__main__':
 
     # model = torch.nn.SyncBatchNorm.convert_sync_batchnorm(model)
     model = torch.nn.parallel.DistributedDataParallel(model,find_unused_parameters=True)
+    train_sampler = RandomSampler(trainset, replacement=True, num_samples=args.data_sample) \
+      if args.data_sample is not None else \
+      torch.utils.data.distributed.DistributedSampler(trainset, shuffle=True)
+    shuffle = True if args.data_sample is None else False
 
     trainloader = DataLoader(trainset, batch_size=args.bs, num_workers=args.num_workers,
                              sampler=train_sampler)

network/EmbeddingNetwork.py
@@ -1,3 +1,5 @@
+from torch import nn
+
 from network.Resnet3dAgg import Encoder3d, Decoder3d, Resnet3dSimilarity
 from network.embedding_head import NonlocalOffsetEmbeddingHead
 from network.models import BaseNetwork
@@ -26,8 +28,10 @@ class DecoderWithEmbedding(Decoder3d):
 
 
 class DecoderSegmentEmbedding(DecoderWithEmbedding):
-  def __init__(self, n_classes=2):
-    super(DecoderSegmentEmbedding, self).__init__(n_classes=n_classes, e_dim=256)
+  def __init__(self, n_classes=2, e_dim=64):
+    super(DecoderSegmentEmbedding, self).__init__(n_classes=n_classes, e_dim=e_dim)
+    # self.convG1 = nn.Conv3d(2048, 256, kernel_size=3, padding=1)
+    self.con1x1 = nn.Conv3d(e_dim, 256, kernel_size=1, padding=1)
 
   def forward(self, r5, r4, r3, r2, support):
     x = self.GC(r5)
@@ -39,7 +43,8 @@ class DecoderSegmentEmbedding(DecoderWithEmbedding):
     m2 = self.RF2(r2, m3)  # out: 1/4, 64
     e = self.embedding_head(F.interpolate(m3, scale_factor=(2, 1, 1), mode='trilinear'))
 
-    p2 = self.pred2(F.relu(m2) + F.interpolate(e, m2.shape[2:], mode='trilinear'))
+    e_unrolled = self.con1x1(F.relu(e))
+    p2 = self.pred2(F.relu(m2) + F.interpolate(e_unrolled, m2.shape[2:], mode='trilinear'))
     p = F.interpolate(p2, scale_factor=(1, 4, 4), mode='trilinear')
 
     return p, e
@@ -53,9 +58,9 @@ class Resnet3dEmbeddingNetwork(Resnet3dSimilarity):
 
 
 class Resnet3dSegmentEmbedding(Resnet3dSimilarity):
-  def __init__(self, tw=8, sample_size=112,n_classes=2):
+  def __init__(self, tw=8, sample_size=112,n_classes=2, e_dim=64):
     super(Resnet3dSegmentEmbedding, self).__init__(n_classes=n_classes)
     self.encoder = Encoder3d(tw, sample_size)
-    self.decoder = DecoderSegmentEmbedding(n_classes=n_classes)
+    self.decoder = DecoderSegmentEmbedding(n_classes=n_classes, e_dim=e_dim)
 
 

network/embedding_head.py
@@ -94,8 +94,9 @@ class NonlocalOffsetEmbeddingHead(nn.Module):
         # x = x[0]
         N, C, T, H, W = x.shape
 
+        # comment: t_scale = 1/(H/T): eg:-352/8
         grid = self.nonlocal_block.create_spatiotemporal_grid(
-            H, W, T, 0.1, x.dtype, x.device).unsqueeze(0).expand(N, -1, -1, -1, -1)  # [N, 3, 1, 1, 1]
+            H, W, T, 0.028, x.dtype, x.device).unsqueeze(0).expand(N, -1, -1, -1, -1)  # [N, 3, 1, 1, 1]
         zeros = torch.zeros(N, C-3, T, H, W, dtype=x.dtype, device=x.device)
         grid_cat = torch.cat((grid, zeros), dim=1)
 
@@ -106,11 +107,11 @@ class NonlocalOffsetEmbeddingHead(nn.Module):
 
         x = self.conv_offset(x)  # [N, 2, T, H, W]
 
-        grid = grid[:, 1:]
-        if self.embedding_size > 2:
-            zeros = torch.zeros(N, self.embedding_size - 2, T, H, W, dtype=x.dtype, device=x.device)
+        # grid = grid[:, 1:]
+        if self.embedding_size > 3:
+            zeros = torch.zeros(N, self.embedding_size - 3, T, H, W, dtype=x.dtype, device=x.device)
             grid_cat = torch.cat((grid, zeros), dim=1)
-        elif self.embedding_size == 2:
+        elif self.embedding_size == 3:
             grid_cat = grid
         else:  # embedding size == 1
             grid_cat = torch.tensor(0, dtype=x.dtype, device=x.device)

train.py
@@ -2,6 +2,7 @@ import os
 import time
 
 import numpy as np
+import sys
 import torch
 from PIL import Image
 from tensorboardX import SummaryWriter
@@ -14,6 +15,7 @@ from network.RGMP import Encoder
 from utils.Argparser import parse_args
 # Constants
 from utils.AverageMeter import AverageMeter
+from utils.Config import Config
 from utils.Constants import DAVIS_ROOT
 from utils.Loss import bootstrapped_ce_loss
 from utils.Saver import load_weights, save_checkpoint
@@ -156,7 +158,7 @@ def compute_loss(criterion, pred, target, target_extra = None):
       loss_extra = bootstrapped_ce_loss(loss_extra)
     if "embedding" in args.losses:
       pred_extra = F.interpolate(pred_extra, scale_factor=(1,8,8), mode='trilinear')
-      loss_extra, _, _ = compute_embedding_loss(pred_extra, target_extra['similarity_ref'].cuda())
+      loss_extra, _, _ = compute_embedding_loss(pred_extra, target_extra['similarity_ref'].cuda(), args.config_path)
 
   # print("loss_extra {}".format(loss_extra))
   loss = loss_mask + loss_extra
@@ -219,13 +221,6 @@ if __name__ == '__main__':
     print("Arguments used: {}".format(args), flush=True)
 
     trainset, testset = get_dataset(args)
-    # trainset = DAVIS(DAVIS_ROOT, imset='2017/train.txt', is_train=True,
-    #                  random_instance=args.random_instance, crop_size=args.crop_size, resize_mode=args.resize_mode)
-    # trainset = YoutubeVOSDataset(YOUTUBEVOS_ROOT, imset='train', is_train=True,
-    #                  random_instance=args.random_instance, crop_size=args.crop_size, resize_mode=args.resize_mode)
-    # testset = DAVISEval(DAVIS_ROOT, imset='2017/val.txt', random_instance=False, crop_size=args.crop_size_eval,
-    #                     resize_mode=args.resize_mode_eval)
-    # sample a subset of data for testing
     sampler = RandomSampler(trainset, replacement=True, num_samples=args.data_sample) \
       if args.data_sample is not None else None
     shuffle = True if args.data_sample is None else False

utils/Saver.py
@@ -50,7 +50,7 @@ def load_weights(model, optimizer, args, model_dir, scheduler):
       missing_keys = np.setdiff1d(list(state.keys()),list(checkpoint_valid.keys()))
       
       if len(missing_keys) > 0:
-        print("WARN: {} keys are found missing in the loaded model weights.".format(len(missing_keys)))
+        print("WARN: {} keys are found missing in the loaded model weights.".format(missing_keys))
       for key in missing_keys:
         checkpoint_valid[key] = state[key]
 

utils/embedding_loss.py
@@ -1,5 +1,10 @@
+import os
+
 import torch
 from torch.nn import functional as F
+
+from utils.Config import Config
+
 
 def smooth_l1_loss(loss):
   return torch.where(loss < 1., torch.pow(loss, 2), loss)
@@ -8,7 +13,7 @@ def make_zero_tensor(dtype, device, required_grad):
   return torch.tensor(0, dtype=dtype, device=device, requires_grad=required_grad)
 
 
-def compute_embedding_loss(embedding_map, targets):
+def compute_embedding_loss(embedding_map, targets, config_path):
   """
   Computes the embedding loss.
   :param embedding_map: Tensor of shape [N, E (embedding dimensionality), T, H, W]
@@ -16,18 +21,21 @@ def compute_embedding_loss(embedding_map, targets):
   shape (I (instances), T, H, W)
   :return: Tuple of 3 losses (only the first value has to be back-proped); the rest are for logging)
   """
+
+  assert os.path.exists(config_path)
+  config = Config(config_path)
   embedding_map = embedding_map.permute(0, 2, 3, 4, 1)  # [N, T, H, W, E]
 
   losses_variance = torch.zeros((0,), dtype=torch.float32, device=embedding_map.device)
   losses_distance = torch.zeros((0,), dtype=torch.float32, device=embedding_map.device)
   losses_regularization = torch.zeros((0,), dtype=torch.float32, device=embedding_map.device)
 
-  DELTA_VAR = 0.02  # distance of instance pixels to their respective centers
-  DELTA_DISTANCE = 0.5  # distance between centers of different instances (/2)
+  DELTA_VAR = config.float("DELTA_VAR", 0.02)  # distance of instance pixels to their respective centers
+  DELTA_DISTANCE = config.float("DELTA_DISTANCE", 0.5)  # distance between centers of different instances (/2)
 
-  W_VAR_LOSS = 0.2
-  W_DISTANCE_LOSS = 0.8
-  W_REGULARIZATION_LOSS = 1e-3
+  W_VAR_LOSS = config.float("W_VAR_LOSS",0.2)
+  W_DISTANCE_LOSS = config.float("W_DISTANCE_LOSS",0.8)
+  W_REGULARIZATION_LOSS = config.float("W_REGULARIZATION_LOSS",1e-3)
 
   # The masks have to be resized to match the size of the embedding map
   SCALE_FACTOR = 1