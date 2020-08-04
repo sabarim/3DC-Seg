5a62967084e7e4bf9a3bb3de86baedb61744750d
sm110193 <sabarinath.mahadevan@rwth-aachen.de>
2020-08-04 12:16:25 +0200

    Dataset restructuring for bmvc-v2

---
+++ datasets/BaseDataset.py
@@ -0,0 +1,157 @@
+import random
+from abc import abstractmethod
+
+import cv2
+import numpy as np
+from PIL import Image
+from imageio import imread
+from utils.Resize import resize, ResizeMode
+from torch.utils.data import Dataset
+
+from utils.util import list_to_dict
+
+
+class BaseDataset(Dataset):
+  def __init__(self, root, mode='train', resize_mode=None, resize_shape=None):
+    self.resize_mode = ResizeMode(resize_mode)
+    self.resize_shape = resize_shape
+    self.mode = mode
+    self.root = root
+    self.samples = []
+    self.create_sample_list()
+
+  # Override in case tensors have to be normalised
+  def normalise(self, tensors):
+    tensors['images'] = tensors['images'].astype(np.float32) / 255.0
+    return tensors
+
+  def is_train(self):
+    return self.mode == "train"
+
+  def pad_tensors(self, tensors_resized):
+    h, w = tensors_resized["images"].shape[:2]
+    new_h = h + 32 - h % 32 if h % 32 > 0 else h
+    new_w = w + 32 - w % 32 if w % 32 > 0 else w
+    lh, uh = (new_h - h) / 2, (new_h - h) / 2 + (new_h - h) % 2
+    lw, uw = (new_w - w) / 2, (new_w - w) / 2 + (new_w - w) % 2
+    lh, uh, lw, uw = int(lh), int(uh), int(lw), int(uw)
+
+    padded_tensors = {}
+    for key, tensor in tensors_resized.items():
+      if tensor.ndim == 2:
+        tensor = tensor[..., None]
+      assert tensor.ndim == 3
+      padded_tensors[key] = np.pad(tensor,
+                                   ((lh, uh), (lw, uw), (0, 0)),
+                                   mode='constant')
+
+    return padded_tensors
+
+  def read_sample(self, sample):
+    images = map(imread, sample['images'])
+    # targets = map(imread, sample['targets'])
+    targets = map(lambda x: np.array( Image.open(x).convert('P'), dtype=np.uint8), sample['targets'])
+
+    images = list_to_dict(images)
+    images = resize(images, self.resize_mode, self.resize_shape)
+    images = np.stack(images.values())
+
+    targets = list_to_dict(targets)
+    targets = resize(targets, self.resize_mode, self.resize_shape)
+    targets = np.stack(targets.values())
+
+    data = {"images": images, "targets": targets}
+    for key, val in sample.items():
+      if key in ['images', 'targets']:
+        continue
+      if key in data:
+        data[key] += [val]
+      else:
+        data[key] = [val]
+    return data
+
+  def __len__(self):
+    return len(self.samples)
+
+  def __getitem__(self, idx):
+    sample = self.samples[idx]
+    tensors_resized = self.read_sample(sample)
+
+    padded_tensors = self.pad_tensors(tensors_resized)
+
+    padded_tensors = self.normalise(padded_tensors)
+
+    return {"images": [np.transpose(padded_tensors['img1'], (2, 0, 1)).astype(np.float32),
+                       np.transpose(padded_tensors['img2'], (2, 0, 1)).astype(np.float32)],
+            "target": {"flow": np.transpose(padded_tensors['flow'], (2, 0, 1)).astype(np.float32)}, 'info': {}}
+
+  @abstractmethod
+  def create_sample_list(self):
+    pass
+
+
+class VideoDataset(BaseDataset):
+  def __init__(self, root, mode='train', resize_mode=None, resize_shape=None, tw=8, max_temporal_gap=8, num_classes=2):
+    self.tw = tw
+    self.max_temporal_gap = max_temporal_gap
+    self.num_classes = num_classes
+
+    self.videos = []
+    self.num_frames = {}
+    self.num_objects = {}
+    self.shape = {}
+
+    self.current_video = None
+    self.start_index = None
+    super(VideoDataset, self).__init__(root, mode, resize_mode, resize_shape)
+
+  def set_video_id(self, video):
+    self.current_video = video
+    self.start_index = self.get_start_index(video)
+
+  def get_video_ids(self):
+    # shuffle the list for training
+    return random.sample(self.videos, len(self.videos)) if self.is_train() else self.videos
+
+  def get_start_index(self, video):
+    start_frame = 0
+    return start_frame
+
+  def pad_tensors(self, tensors_resized):
+    h, w = tensors_resized["images"].shape[1:3]
+    new_h = h + 32 - h % 32 if h % 32 > 0 else h
+    new_w = w + 32 - w % 32 if w % 32 > 0 else w
+    lh, uh = (new_h - h) / 2, (new_h - h) / 2 + (new_h - h) % 2
+    lw, uw = (new_w - w) / 2, (new_w - w) / 2 + (new_w - w) % 2
+    lh, uh, lw, uw = int(lh), int(uh), int(lw), int(uw)
+
+    padded_tensors = tensors_resized.copy()
+    keys = ['images', 'targets']
+
+    for key in keys:
+      pt = []
+      t = tensors_resized[key]
+      if t.ndim == 3:
+        t = t[..., None]
+      assert t.ndim == 4
+      padded_tensors[key] = np.pad(t,
+                   ((0,0),(lh, uh), (lw, uw), (0, 0)),
+                   mode='constant')
+
+    return padded_tensors
+
+  def __getitem__(self, idx):
+    sample = self.samples[idx]
+    tensors_resized = self.read_sample(sample)
+
+    padded_tensors = self.pad_tensors(tensors_resized)
+
+    padded_tensors = self.normalise(padded_tensors)
+
+    return {"images": np.transpose(padded_tensors['images'], (3, 0, 1, 2)).astype(np.float32),
+            "target": {"mask": np.transpose(padded_tensors['targets'], (3, 0, 1, 2)).astype(np.float32)},
+            'info': padded_tensors['info']}
+
+  @abstractmethod
+  def get_support_indices(self, index, sequence):
+    pass
---
+++ datasets/davis/Davis.py
@@ -0,0 +1,107 @@
+import glob
+import os
+import random
+import numpy as np
+from PIL import Image
+
+from datasets.BaseDataset import VideoDataset
+from utils.Resize import ResizeMode
+
+TARGETS = 'targets'
+IMAGES_ = 'images'
+INFO = 'info'
+
+
+class Davis(VideoDataset):
+  def __init__(self, root, mode='train', resize_mode=None, resize_shape=None, tw=8, max_temporal_gap=8, num_classes=2,
+               imset=None):
+    self.imset = imset
+    self.videos = []
+    self.num_frames = {}
+    self.num_objects = {}
+    self.shape = {}
+    super(Davis, self).__init__(root, mode, resize_mode, resize_shape, tw, max_temporal_gap, num_classes)
+
+  def filter_samples(self, video):
+    filtered_samples = [s for s in self.samples if s[INFO]['video'] == video]
+    self.samples = filtered_samples
+
+  def set_video_id(self, video):
+    self.current_video = video
+    self.start_index = self.get_start_index(video)
+    self.filter_samples(video)
+
+  def get_video_ids(self):
+    # shuffle the list for training
+    return random.sample(self.videos, len(self.videos)) if self.is_train else self.videos
+
+  def get_support_indices(self, index, sequence):
+    # index should be start index of the clip
+    if self.is_train():
+      index_range = np.arange(index, min(self.num_frames[sequence],
+                                         (index + max(self.max_temporal_gap, self.tw))))
+    else:
+      index_range = np.arange(index,
+                              min(self.num_frames[sequence], (index + self.tw)))
+
+    support_indices = np.random.choice(index_range, min(self.tw, len(index_range)), replace=False)
+    support_indices = np.sort(np.append(support_indices, np.repeat([index],
+                                                                   self.tw - len(support_indices))))
+
+    print(support_indices)
+    return support_indices
+
+  def create_sample_list(self):
+    image_dir = os.path.join(self.root, 'JPEGImages', '480p')
+    mask_dir = os.path.join(self.root, 'Annotations_unsupervised', '480p')
+    if self.imset:
+      _imset_f = self.imset
+    elif self.is_train():
+      _imset_f = '2017/train.txt'
+    else:
+      _imset_f = '2017/val.txt'
+
+    with open(os.path.join(self.root, "ImageSets",_imset_f), "r") as lines:
+      for line in lines:
+        _video = line.rstrip('\n')
+        img_list = list(glob.glob(os.path.join(image_dir, _video, '*.jpg')))
+        img_list.sort()
+        # self.videos.append(_video)
+
+        num_frames = len(glob.glob(os.path.join(image_dir, _video, '*.jpg')))
+        self.num_frames[_video] = num_frames
+
+        _mask_file = os.path.join(mask_dir, _video, '00000.png')
+        _mask = np.array(Image.open(os.path.join(mask_dir, _video, '00000.png')).convert("P"))
+        num_objects = np.max(_mask)
+        self.num_objects[_video] = num_objects
+        self.shape[_video] = np.shape(_mask)
+
+        for i, img in enumerate(img_list):
+          sample = {INFO: {}, IMAGES_: [], TARGETS: []}
+          support_indices = self.get_support_indices(i, _video)
+          sample[INFO]['support_indices'] = support_indices
+          images = [os.path.join(image_dir, _video, '{:05d}.jpg'.format(s)) for s in np.sort(support_indices)]
+          targets = [os.path.join(mask_dir, _video, '{:05d}.png'.format(s)) for s in np.sort(support_indices)]
+          sample[IMAGES_] = images
+          sample[TARGETS] = targets
+
+          sample[INFO]['video'] = _video
+          sample[INFO]['num_frames'] = num_frames
+          sample[INFO]['num_objects'] = num_objects
+          sample[INFO]['shape'] = np.shape(_mask)
+
+          self.samples+=[sample]
+
+
+if __name__ == '__main__':
+    davis = Davis(root="/globalwork/data/DAVIS-Unsupervised/DAVIS/",
+                  resize_shape=(480, 854), resize_mode=ResizeMode.FIXED_SIZE, mode="train", max_temporal_gap=32)
+
+    # davis.set_video_id('cat-girl')
+    print("Dataset size: {}".format(davis.__len__()))
+
+    for i, _input in enumerate(davis):
+      print(_input['info'])
+      print("Image Max {}, Image Min {}".format(_input['images'].max(), _input['images'].min()),
+            "Target max {}, Target Min {}".format(_input['target']['mask'].max(), _input['target']['mask'].min()))
